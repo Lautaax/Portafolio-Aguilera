44b51d2ce3eca914243544ee9dc0106f092675df
Lautaax <lautaa_97@outlook.com>
2023-03-07 01:59:36 -0300

    cambios

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,27 +1,17 @@
 import React from 'react'
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import QrAguilera from "../Images/Qraguilerasvg.svg"
 import "./Contact.css"
+
 
 export const Contact = () => {
   return (
-    <div style={{   height: "64vh" }}>
-<div >
-            <Form  style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }} className='container w-25'>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                    <Form.Label className='nombre' >Nombre</Form.Label>
-                    <Form.Control name='nombre' type="text" placeholder="Jaimito" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                    <Form.Label className='email' >Email address</Form.Label>
-                    <Form.Control name='mail' type="email" placeholder="name@example.com" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                    <Form.Label className='telefono'>Telefono</Form.Label>
-                    <Form.Control name='telefono' type="number" placeholder="+5421352456" />
-                </Form.Group>
-                <Button variant="success" type='submit' >Enviar Datos</Button>{' '}
-            </Form>
+    <div style={{   height: "69vh" }}>
+<div className='image' >
+<img 
+alt='...'
+className='qr'
+src={QrAguilera}/>
+
         </div>
 
     </div>
